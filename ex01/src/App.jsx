1a23f7a586594b0b44b35dd1e7fb805b9e714655
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2026-04-21 00:17:01 -0300

    Criando repositorio

---
+++ ex01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/App.css";
+function App() {
+    return (
+         <div className="titulo">
+             <h2>Acesso à informação</h2>
+            <p>Veja dados de transparência e governança.</p>
+         </div>
+          );
+}
+
+export default App;
